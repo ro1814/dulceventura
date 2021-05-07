20b6e4eec883d9c7cc7e96fd2e3180ad703db98d
Rodolfo S <rnsm14@gmail.com>
2021-05-07 10:45:57 +0200

    Envireonment set for deployment, ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,53 @@
-import path from 'path'
-import express from 'express'
-import dotenv from 'dotenv'
-import colors from 'colors'
-import morgan from 'morgan'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-import connectDB from './config/db.js'
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import morgan from "morgan";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 
-import recipeRoutes from './routes/recipeRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import uploadRoutes from './routes/uploadRoutes.js'
+import recipeRoutes from "./routes/recipeRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
+dotenv.config();
 
+connectDB();
 
-dotenv.config()
+const app = express();
 
-connectDB()
-
-const app = express()
-
-if(process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
-})
+app.use("/api/recipes", recipeRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/upload", uploadRoutes);
 
-app.use('/api/recipes', recipeRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/upload', uploadRoutes)
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-app.use(notFound)
-app.use(errorHandler)
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
-const PORT= process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
